dcbdcc997794842cc62cdd8a266c16f6312ffb40
nelcael07 <47829164+nelcael07@users.noreply.github.com>
2021-11-19 16:36:14 -0400

    feat: implementação do get de usuarios.

--- src/main/java/com/example/demo/api/controller/UsuarioController.java
+++ src/main/java/com/example/demo/api/controller/UsuarioController.java
@@ -1,7 +1,10 @@
 package com.example.demo.api.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +25,11 @@ public class UsuarioController {
 	@ResponseStatus(HttpStatus.CREATED)
 	public void criar(@RequestBody Usuario usuario) {
 		usuarioRepository.save(usuario);
+	}
+	
+	@GetMapping
+	public List<Usuario> listar() {
+		return usuarioRepository.findAll();
 	}
 	
 }
